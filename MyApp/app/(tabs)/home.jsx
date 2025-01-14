37cf05560eded225f93338ae69e70d1fc5bfdf5b
Quan Quan <79500388+QuanNguyenDong@users.noreply.github.com>
2025-01-14 16:15:34 +1100

    display fake users

--- MyApp/app/(tabs)/home.jsx
+++ MyApp/app/(tabs)/home.jsx
@@ -1,36 +1,70 @@
 import { useEffect, useState } from "react";
-import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 const Item = ({ item }) => (
   <View style={styles.item}>
-    <Text>Title: {item.title}</Text>
-    <Text>Desc: {item.description}</Text>
-    <Text>Ingredients: {item.ingredients.join(", ")}</Text>
-    <View>
-      <Image style={{ width: 300, height: 300 }} source={{ uri: item.image }} />
-    </View>
+    <Text>Name: {item.name}</Text>
+    <Text>Geolocation: {item.address?.geo?.lat}</Text>
+    <Text>Geolocation: {item.address?.geo?.lng}</Text>
   </View>
 );
 
 export default function Home() {
-  const [coffee, setCoffee] = useState([]);
+  const [users, setUsers] = useState([]);
 
-  const getCoffee = async () => {
+  const API_URL = "https://testvm1.rokt.io/api/jsonql";
+  // should be env var
+  const API_KEY = "c37861c7-7414-4a40-bbd8-3343662e4483";
+
+  const getFakeUsers = async () => {
     try {
-      const res = await fetch("https://api.sampleapis.com/coffee/hot");
-      const json = await res.json();
-      setCoffee(json);
-    } catch (err) {}
+      const res = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": API_KEY,
+        },
+        body: JSON.stringify({
+          fakeUsers: {},
+        }),
+      });
+
+      if (!res.ok) {
+        throw Error("Invalid Request");
+      }
+
+      const fakeUsers = await res.json();
+      const arr = Array.from(fakeUsers["fakeUsers"]);
+
+      // sort geo
+      arr.sort((user1, user2) => {
+        const lat1 = user1.address?.geo?.lat
+        const lng1 = user1.address?.geo?.lng
+        
+        const lat2 = user2.address?.geo?.lat
+        const lng2 = user2.address?.geo?.lng
+
+        // euclidean distance
+        const d1 = Math.sqrt((lat1 * lat1) + (lng1 * lng1))
+        const d2 = Math.sqrt((lat2 * lat2) + (lng2 * lng2))
+
+        return d1 - d2;
+      })
+
+      setUsers(arr);
+    } catch (err) {
+      console.log(err)
+    }
   };
 
   useEffect(() => {
-    getCoffee();
-  }, [coffee]);
+    getFakeUsers()
+  }, []);
 
   return (
     <View>
       <FlatList
-        data={coffee}
+        data={users}
         style={{ height: "100%" }}
         renderItem={({ item }) => <Item item={item} />}
         keyExtractor={(item) => item.id}
